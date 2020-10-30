c36ad74e079385949a37d9433790133f3ab8eec5
shrihalyal <52747935+shrihalyal@users.noreply.github.com>
2020-10-30 10:58:29 -0400

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 /* This is a simple Java program. 
 FileName : "HelloWorld.java". */
-public class HelloWorld 
+public class Hello
 { 
 	// Your program begins with a call to main(). 
 	// Prints "Hello, World" to the terminal window. 
